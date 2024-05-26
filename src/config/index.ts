d3d8ae91f1fb34d38476393a39d1a67d6c18b080
MDMahidul <mmbmahidul007@gmail.com>
2024-05-26 11:36:01 +0600

    basic setup done

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+};

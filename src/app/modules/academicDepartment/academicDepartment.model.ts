0e95bcc126992a93c4162af93247225b7f6b197a
MDMahidul <mmbmahidul007@gmail.com>
2024-06-01 00:53:17 +0600

    fixed update validation and populate data referencing fields

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,18 +1,39 @@
-import { Schema, model } from 'mongoose';
-import { TAcademicDepartment } from './academicDepartment.interface';
+import { Schema, model } from "mongoose";
+import { TAcademicDepartment } from "./academicDepartment.interface";
 
 const academicDepartmentSchema = new Schema(
   {
     name: { type: String, unique: true, required: true },
-    academicFaculty:{
-        type:Schema.Types.ObjectId,
-        ref:'AcademicFaculty'
-    }
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicFaculty",
+    },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
+academicDepartmentSchema.pre("save", async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) {
+    throw new Error("This department already exist!");
+  }
+  next();
+});
+
+academicDepartmentSchema.pre("findOneAndUpdate", async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) {
+    throw new Error("This department does not exist!");
+  }
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
-  'AcademicDepartment',
-  academicDepartmentSchema,
+  "AcademicDepartment",
+  academicDepartmentSchema
 );
--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -58,6 +58,7 @@ const createStudentValidationSchema = z.object({
       localGuardian: localGuardianValidationSchema,
       profileImage: z.string().optional(),
       admissionSemester: z.string(),
+      academicDepartment: z.string(),
     }),
   }),
 });

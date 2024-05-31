3b39828c2b15aba099f43a2fa75d0142477e7367
MDMahidul <mmbmahidul007@gmail.com>
2024-05-31 10:43:45 +0600

    fixed generate student Id for different semester and year

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -5,18 +5,29 @@ import { User } from "./user.model"
 const findLastStudentId = async()=>{
     const lastStudent = await User.findOne({role:'student'},{id:1,_id:0}).sort({createdAt:-1}).lean();
 
-    return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent.id: undefined;
 }
 
 // generate id
 export const generateStudentId = async(payload:TAcademicSemester)=>{
-    // first student entry
-    const currentId = await findLastStudentId() || (0).toString();
+  // first student entry
+  let currentId = (0).toString(); // by default 0000
 
-    let incrementId = (Number(currentId) + 1).toString().padStart(4,'0');
+  const lastStudentId = await findLastStudentId(); //get the last student Id
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+  // get the current student semester code and year from payload
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
 
-    // create the id
-    incrementId = `${payload.year}${payload.code}${incrementId}`;
+  if(lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentYear){
+    currentId = lastStudentId.substring(6);
+  }
 
-    return incrementId;
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
+
+  // create the id
+  incrementId = `${payload.year}${payload.code}${incrementId}`;
+
+  return incrementId;
 }
